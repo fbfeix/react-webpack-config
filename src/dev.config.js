72e81a67b15635768428c3e3f3ab0601166c8c87
Felix Astner <astner.felix@live.de>
2018-03-02 13:03:16 +0100

    formating

--- src/dev.config.js
+++ src/dev.config.js
@@ -1,92 +1,98 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    app: "./src/index.js"
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
-    contentBase: './dist'
+    contentBase: "./dist"
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(["dist"]),
     new HtmlWebpackPlugin({
-      title: 'Development'
+      title: "Development"
     })
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
 
   module: {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel-loader'
+        loader: "babel-loader"
       },
       {
-          test: /\.(scss|css)$/,
-          use: [
-              {loader: 'style-loader?sourceMap'},
-              {loader: 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]--[hash:base64:5]'},
-              {
-                  loader: 'postcss-loader?sourceMap',
-                  options: {
-                      autoprefixer: true,
-                      sourceMap: true
-                  }
-              },
-              {loader: 'resolve-url-loader'},
-              {loader: 'sass-loader?sourceMap'}
-          ]
+        test: /\.(scss|css)$/,
+        use: [
+          { loader: "style-loader?sourceMap" },
+          {
+            loader:
+              "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]--[hash:base64:5]"
+          },
+          {
+            loader: "postcss-loader?sourceMap",
+            options: {
+              autoprefixer: true,
+              sourceMap: true
+            }
+          },
+          { loader: "resolve-url-loader" },
+          { loader: "sass-loader?sourceMap" }
+        ]
       },
       {
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        loader: "url-loader?limit=10000&mimetype=application/font-woff",
         query: {
-            name: '[name].min.[ext]?[hash]',
-            outputPath: "fonts/", // path is combined by output.path + fileloader.outputPath
-            publicPath: "/" // + outputPath
+          name: "[name].min.[ext]?[hash]",
+          outputPath: "fonts/", // path is combined by output.path + fileloader.outputPath
+          publicPath: "/" // + outputPath
         }
-    },
-    {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream",
         query: {
-            name: '[name].min.[ext]?[hash]',
-            outputPath: "fonts/", // path is combined by output.path + fileloader.outputPath
-            publicPath: "/" // + outputPath
+          name: "[name].min.[ext]?[hash]",
+          outputPath: "fonts/", // path is combined by output.path + fileloader.outputPath
+          publicPath: "/" // + outputPath
         }
-    },
-    {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader',
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader",
         query: {
-            name: '[name].min.[ext]?[hash]',
-            outputPath: "fonts/", // path is combined by output.path + fileloader.outputPath
-            publicPath: "/" // + outputPath
+          name: "[name].min.[ext]?[hash]",
+          outputPath: "fonts/", // path is combined by output.path + fileloader.outputPath
+          publicPath: "/" // + outputPath
         }
-    },
-    {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml",
         query: {
-            name: '[name].min.[ext]?[hash]',
-            outputPath: "fonts/", // path is combined by output.path + fileloader.outputPath
-            publicPath: "/" // + outputPath
+          name: "[name].min.[ext]?[hash]",
+          outputPath: "fonts/", // path is combined by output.path + fileloader.outputPath
+          publicPath: "/" // + outputPath
         }
-    },
-    {
+      },
+      {
         test: /\.(jpg|jpeg|png|svg)$/,
         use: {
-            loader: 'file-loader',
-            query: {
-                useRelativePath: process.env.NODE_ENV === "production",
-                outputPath: "imgs/", // path is combined by output.path + fileloader.outputPath
-                publicPath: "/" // + outputPath
-            }
+          loader: "file-loader",
+          query: {
+            useRelativePath: process.env.NODE_ENV === "production",
+            outputPath: "imgs/", // path is combined by output.path + fileloader.outputPath
+            publicPath: "/" // + outputPath
+          }
         }
-    }
+      }
     ]
   }
 };
--- src/production.config.js
+++ src/production.config.js
@@ -1,70 +1,115 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const glob = require('glob')
-const webpack = require('webpack')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const glob = require("glob");
+const webpack = require("webpack");
 
-const HeaderCssFileExtractor = new ExtractTextPlugin('style/header.min.css?[hash]');
+const HeaderCssFileExtractor = new ExtractTextPlugin(
+  "style/header.min.css?[hash]"
+);
 
 let items = {};
-for(let file of  glob.sync("./src/components/**/!(*.test).js")){
-  const key = path.basename(file, '.js')
-console.log(file)
+for (let file of glob.sync("./src/components/**/!(*.test).js")) {
+  const key = path.basename(file, ".js");
+  console.log(file);
   items[key.toLowerCase()] = file;
 }
 
 module.exports = {
   entry: items,
 
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
-    contentBase: './dist'
+    contentBase: "./dist"
   },
 
   output: {
-    filename: '[name].js?[hash]',
-    path: path.resolve(__dirname, 'dist')
+    filename: "[name].js?[hash]",
+    path: path.resolve(__dirname, "dist")
   },
 
   module: {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel-loader'
+        loader: "babel-loader"
       },
       {
         test: /\.(scss|css)$/,
         use: HeaderCssFileExtractor.extract({
           fallback: "style-loader",
           use: [
-
             {
-              loader: 'css-loader', //?modules&importLoaders=1&localIdentName=[hash:base64:5]
+              loader: "css-loader", //?modules&importLoaders=1&localIdentName=[hash:base64:5]
               options: {
                 modules: true,
                 importLoaders: 1,
                 localIdentName: "[hash:base64:8]"
               }
-
             },
 
             {
-              loader: 'postcss-loader',
+              loader: "postcss-loader",
               options: {
                 autoprefixer: true,
                 sourceMap: false
               }
             },
-            { loader: 'resolve-url-loader' },
-            { loader: 'sass-loader' }
+            { loader: "resolve-url-loader" },
+            { loader: "sass-loader" }
           ]
         })
-
       },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff",
+        query: {
+          name: "[name].min.[ext]?[hash]",
+          outputPath: "fonts/", // path is combined by output.path + fileloader.outputPath
+          publicPath: "/" // + outputPath
+        }
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream",
+        query: {
+          name: "[name].min.[ext]?[hash]",
+          outputPath: "fonts/", // path is combined by output.path + fileloader.outputPath
+          publicPath: "/" // + outputPath
+        }
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader",
+        query: {
+          name: "[name].min.[ext]?[hash]",
+          outputPath: "fonts/", // path is combined by output.path + fileloader.outputPath
+          publicPath: "/" // + outputPath
+        }
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml",
+        query: {
+          name: "[name].min.[ext]?[hash]",
+          outputPath: "fonts/", // path is combined by output.path + fileloader.outputPath
+          publicPath: "/" // + outputPath
+        }
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)$/,
+        use: {
+          loader: "file-loader",
+          query: {
+            useRelativePath: process.env.NODE_ENV === "production",
+            outputPath: "imgs/", // path is combined by output.path + fileloader.outputPath
+            publicPath: "/" // + outputPath
+          }
+        }
+      }
     ]
   },
-
 
   // plugins: [
   //   new CleanWebpackPlugin(['dist']),
@@ -74,16 +119,16 @@ module.exports = {
   // ],
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
-      }
-      , sourceMap: false
+      },
+      sourceMap: false
     }),
-    HeaderCssFileExtractor,
-  ],
+    HeaderCssFileExtractor
+  ]
 };
